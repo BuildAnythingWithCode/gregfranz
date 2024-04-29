ee98c2ecf52683f1aea83b550d71d98de8075180
BuildAnythingWithCode <buildanythingwithcode@gmail.com>
2024-04-30 02:16:03 +0800

    landing page done

---
+++ script.js
@@ -0,0 +1,36 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+
+
+/*
+let sections = document.querySelectorAll('section');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 550;
+    let height = sec.offsetHeight;
+
+    if (top >= offset && top < offset + height) {
+      sec.classList.add('show-animate');
+    }
+    else {
+      sec.classList.remove('show-animate');
+    }
+  })
+}
+*/
